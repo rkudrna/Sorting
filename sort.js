e9938c4e2a87029f401216570da86f4d0fee7b4c
Rachel Kudrna <rkudrna@gmail.com>
2014-09-27 16:47:56 -0700

    minor updates to js

--- sort.js
+++ sort.js
@@ -6,8 +6,9 @@ $(function() {
     this.gLoad = gLoad;
     this.calories = calories;
     
+    //add id of fruit's name to each object
     this.toHTML = function() {
-      return '<li><strong>' + 
+      return '<li' + ' id="' + this.name + '"><strong>' + 
       this.name + 
       '</strong><br><em>GL: ' + this.gLoad + 
       '<br>cals: ' + this.calories + 
@@ -32,12 +33,31 @@ $(function() {
   var grape = new Fruit('Grape',9,59);
 
 //create array of Fruit objects
-  var getFruit = $("li");
-  var fruitArray = $.makeArray(getFruit);
-  //var fruitArray = [apple, pear, banana, peach];
+
+//I want to do this dynamically...?
+  //var getFruit = $("li");
+  //var fruitArray = $.makeArray(getFruit);
+  var fruitArray = [apple, pear, banana, peach];
 
 console.log(fruitArray);
-//
+
+var sortByCalories = function() {
+  for (i = 0; i < fruitArray.length-1; i++) {
+    for (j = i+1; j < fruitArray.length; j++) {
+      if (fruitArray[i].calories > fruitArray[j].calories) {
+        temp = fruitArray[i]
+        fruitArray[i] = fruitArray[j]
+        fruitArray[j] = temp
+      }
+    }
+  };
+
+  for (i = 0; i < fruitArray.length; i++) {
+    console.log(fruitArray[i].name + " | calories: " +fruitArray[i].calories);
+  };
+};
+
+sortByCalories();
 
 
   $('button').click(function() {
